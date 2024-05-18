93e31317d159fec20cf0a8aa3f6bff8773fa29fc
cnm37@naver.com <cnm37@naver.com>
2024-05-18 19:19:32 +0900

    Setting

---
+++ src/main/java/com/example/cafe/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.example.cafe.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class WebController {
+
+    @RequestMapping("/")
+    public String MainWeb() {
+        return "home.html";
+    }
+}
