18de8ee7433edd27f41738015be4cc797f8fc2b1
cnm37@naver.com <cnm37@naver.com>
2024-10-25 17:44:35 +0900

    [Feat] notice - 테이블 페이징처리

--- src/main/java/com/example/cafe/Repository/Notice/NoticeRepositoryCustom.java
+++ src/main/java/com/example/cafe/Repository/Notice/NoticeRepositoryCustom.java
@@ -1,11 +1,15 @@
 package com.example.cafe.Repository.Notice;
 
+import com.example.cafe.dto.NoticeDto;
 import com.example.cafe.entity.Notice;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface NoticeRepositoryCustom {
-    public List<Notice> findByTitle(String noticeTitle);
-    public List<Notice> findByDate(LocalDate startDate, LocalDate endDate);
+    public Page<NoticeDto> findAllNotice(Pageable pageable);
+    public Page<NoticeDto> findByTitle(String noticeTitle, Pageable pageable);
+    public Page<NoticeDto> findByDate(LocalDate startDate, LocalDate endDate, Pageable pageable);
 }
