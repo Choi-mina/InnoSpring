6600a2492373b98ce26bac712e99676b09e9020d
cnm37@naver.com <cnm37@naver.com>
2025-01-06 15:36:55 +0900

    [Feat] 인증여부에 따른 로그인/로그아웃 버튼 표시

--- src/main/java/com/example/cafe/config/SecurityConfig.java
+++ src/main/java/com/example/cafe/config/SecurityConfig.java
@@ -24,21 +24,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // 요청 경로 권한 설정
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
                         .requestMatchers("/", "/sign-up-web").permitAll()
                         .anyRequest().authenticated()
                 )
-//                .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer.sessionFixation().migrateSession()
-//                )
 //                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
 ////                .authorizeHttpRequests(authorize -> authorize
 ////                        .requestMatchers("/", "/logout", "/redis/**").permitAll() // 접근 허용
 ////                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
 ////                )
-                // 세션 관리 정책 설정: 세션 무효화 강제 설정
+                // 세션 관리 설정
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) // 항상 세션을 생성
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // 필요한 경우 세션 생성
+                        .sessionFixation().newSession() // 로그인 시 새로운 세션 생성
                         .invalidSessionUrl("/login-web") // 세션이 무효화되면 이 URL로 리다이렉트
                 )
 
@@ -49,8 +49,10 @@ public class SecurityConfig {
                         .passwordParameter("userPassword")  // password 필드를 userPassword로 매핑
                         .defaultSuccessUrl("/", true) // 로그인 성공 후 리다이렉트 URL
                 )
-                .authenticationProvider(authenticationProvider)
+                .authenticationProvider(authenticationProvider) // 인증 프로바이더 설정
+                // 로그아웃 설정
                 .logout(AbstractHttpConfigurer::disable)
+                // csrf 설정
                 .csrf(csrf -> csrf
                         .ignoringRequestMatchers("/login", "/login/**", "/member/sign-up") // 특정 URL에 대해 CSRF 예외 처리
                 )
